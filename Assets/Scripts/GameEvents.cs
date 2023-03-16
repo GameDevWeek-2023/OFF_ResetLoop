e09e8b33370c2d4a7865dc92c3715a214ed6ff99
SabrinaZiebarthFAL <78349894+SabrinaZiebarthFAL@users.noreply.github.com>
2023-03-16 17:25:55 +0100

    Switching Scenes and keeping state

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -17,7 +17,8 @@ public class GameEvents : MonoBehaviour
     [CanBeNull] public Action OnInventoryItemConsumed;
     [CanBeNull] public Action<Position> OnMovePlayerToPosition;
     [CanBeNull] public Action<WorldState.Scene> OnSceneChange;
-    
+    [CanBeNull] public Action<WorldState.KeyEvent> OnKeyEvent;
+
     [CanBeNull] public Action OnWorldReset;
 
     private void Awake()
--- Assets/Scripts/Interaction/BusInteraction.cs
+++ Assets/Scripts/Interaction/BusInteraction.cs
@@ -35,6 +35,6 @@ public class BusInteraction : ItemInteraction
 
     public void WakeBegger()
     {
-        begger.WakeBeggar();
+        begger.WakeBeggar(true);
     }
 }
