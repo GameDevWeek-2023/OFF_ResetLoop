b1be659b0288f41b16d060cf78f318f25bfc3d0a
joschjava <js-2018@posteo.de>
2023-03-19 01:33:49 +0100

    Tool tip hack

---
+++ Assets/Scripts/SimpleHandTooltip.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleHandTooltip : MonoBehaviour
+{
+    [SerializeField] private GameObject tooltip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnMouseEnter()
+    {
+        tooltip.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        tooltip.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
